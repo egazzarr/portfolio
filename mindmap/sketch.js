17aa485fcff3b8364ea1893b2e4875f06b1aa272
egazzarr <56166621+egazzarr@users.noreply.github.com>
2023-02-03 16:29:50 +0100

    add to sketch

--- mindmap/sketch.js
+++ mindmap/sketch.js
@@ -1,22 +1,22 @@
 
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-// }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
 
 
 
-// function draw() {
-//   if (mouseIsPressed) {
-//     fill(0);
-//   } else {
-//     fill(255);
-//   }
-//   ellipse(mouseX, mouseY, 80, 80);
-// }
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
 
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 function setup() {
   createCanvas(710, 400, WEBGL);
