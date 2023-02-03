b3ef41ac011707c0a5f31a9ee4e1a3b56946b5ce
egazzarr <56166621+egazzarr@users.noreply.github.com>
2023-02-03 16:49:42 +0100

    change sketch mindmap

--- mindmap/sketch.js
+++ mindmap/sketch.js
@@ -3,76 +3,74 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-
-
 function draw() {
   if (mouseIsPressed) {
     fill(0);
   } else {
-    fill(255);
+    fill(255, 165, 0);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, 30, 30);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function setup() {
-  createCanvas(710, 400, WEBGL);
-}
+// function setup() {
+//   createCanvas(710, 400, WEBGL);
+// }
 
-function draw() {
-  background(250);
+// function draw() {
+//   background(250);
 
-  translate(-240, -100, 0);
-  normalMaterial();
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  plane(70);
-  pop();
+//   translate(-240, -100, 0);
+//   normalMaterial();
+//   push();
+//   rotateZ(frameCount * 0.01);
+//   rotateX(frameCount * 0.01);
+//   rotateY(frameCount * 0.01);
+//   plane(70);
+//   pop();
 
-  translate(240, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  box(70, 70, 70);
-  pop();
+//   translate(240, 0, 0);
+//   push();
+//   rotateZ(frameCount * 0.01);
+//   rotateX(frameCount * 0.01);
+//   rotateY(frameCount * 0.01);
+//   box(70, 70, 70);
+//   pop();
 
-  translate(240, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  cylinder(70, 70);
-  pop();
+//   translate(240, 0, 0);
+//   push();
+//   rotateZ(frameCount * 0.01);
+//   rotateX(frameCount * 0.01);
+//   rotateY(frameCount * 0.01);
+//   cylinder(70, 70);
+//   pop();
 
-  translate(-240 * 2, 200, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  cone(70, 70);
-  pop();
+//   translate(-240 * 2, 200, 0);
+//   push();
+//   rotateZ(frameCount * 0.01);
+//   rotateX(frameCount * 0.01);
+//   rotateY(frameCount * 0.01);
+//   cone(70, 70);
+//   pop();
 
-  translate(240, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  torus(70, 20);
-  pop();
+//   translate(240, 0, 0);
+//   push();
+//   rotateZ(frameCount * 0.01);
+//   rotateX(frameCount * 0.01);
+//   rotateY(frameCount * 0.01);
+//   torus(70, 20);
+//   pop();
 
-  translate(240, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  sphere(70);
-  pop();
-}
+//   translate(240, 0, 0);
+//   push();
+//   rotateZ(frameCount * 0.01);
+//   rotateX(frameCount * 0.01);
+//   rotateY(frameCount * 0.01);
+//   sphere(70);
+//   pop();
+// }
 
  
